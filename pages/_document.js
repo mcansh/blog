6132bdd9c1472beb28de25f88dbc1251d98da804
Logan McAnsh <logan@mcan.sh>
2017-06-12 15:20:01 -0400

    swap out layout.js for _document to hopefully remove the FOUC (flash of unstyled content)

---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import flush from 'styled-jsx/server';
+import Document, { Head, Main, NextScript } from 'next/document';
+// import Normalize from '../components/css/Normalize';
+// import BodyStyles from '../components/css/BodyStyles';
+import Progress from '../components/post/Progress';
+
+export default class MyDocument extends Document {
+  static async getInitialProps({ renderPage }) {
+    const page = renderPage();
+    const styles = flush();
+    return { ...page, styles };
+  }
+
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <title>Logan McAnsh</title>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link rel="mask-icon" href="/static/images/website_icon.svg" color="#E53A40" />
+          <link rel="shortcut icon" href="/static/images/favicon.png" />
+          {/* <Normalize /> */}
+          {/* <BodyStyles /> */}
+          <link rel="stylesheet" href="/static/css/style.css" />
+        </Head>
+        <body>
+          <Progress />
+          <Main />
+          <NextScript />
+          <script src="/static/js/main.js" />
+        </body>
+      </html>
+    );
+  }
+}
