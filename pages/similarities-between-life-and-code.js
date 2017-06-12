9cd771cadecee01c301c99ad8b9d9d8232ee7be5
Logan McAnsh <logan@mcan.sh>
2017-06-11 20:35:38 -0400

    make emojis accessible

--- pages/similarities-between-life-and-code.js
+++ pages/similarities-between-life-and-code.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import Head from 'next/head';
 import Layout from './Layout';
 import Header from '../components/Header';
@@ -15,7 +14,7 @@ const Index = () => (
 
       <P>Learning to code is like learning anything in life, it takes practice. At the beginning you may feel uncomfortable or like you don’t think you know what you’re doing. That’s actually somewhat of a good thing. As long as you keep practicing you’ll become great. Practice makes perfect. I’m sure you couldn’t ride a bike on the first attempt either, same thing with coding. After a bit of practice you could ride that bike no problem, same concept applies here. Keep on hacking.</P>
 
-      <P>Yeah, I know, another short post *sigh*. Who knows maybe I’ll keep this blogging thing up and going and get better. *gasp* The same process can be made with coding and blogging 😱.</P>
+      <P>Yeah, I know, another short post *sigh*. Who knows maybe I’ll keep this blogging thing up and going and get better. *gasp* The same process can be made with coding and blogging <span role="img" aria-label="Scream Emoji">😱</span>.</P>
       <style jsx>{`
         a {
           color: #E53A40;
--- pages/sinatra-project.js
+++ pages/sinatra-project.js
@@ -15,7 +15,7 @@ const Index = () => (
 
       <P>The first thing you’re greated with is a simple homepage with a link to sign up. Clicking that takes you to a form which you’ll fill out a username, email, and password. Filling out that and submitted, granted there’s no errors, you’ll be greated with a list of your Tasks, which you dont have yet, so maybe I’ll update and put a link there to make a new one. Clicking the button in the bottom right hand corner will take you to a page to make a new Task. Doing so will give you the option for a title and marking it as complete if you completed and just wanted the satisfaction of writing it down. Creating the task will take you back to the homepage, and now you should have that task in your list of Tasks. Clicking on the one you just made will give you the option to edit or delete that task. Editing the task will look very similar to what you just saw when you created one. If you mark it as complete, the task will be crossed out on your list. Deleting the task will delete it and take you back to your homepage.</P>
 
-      <P>That’s all for now <span role="img" aria-label="wave Emoji">👋</span></P>
+      <P>That’s all for now <span role="img" aria-label="Wave Emoji">👋</span></P>
 
       <style jsx>{`
         a {
