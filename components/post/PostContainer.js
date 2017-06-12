4376a36439f2ad16f580fb848cc598459db60e26
Logan McAnsh <logan@mcan.sh>
2017-06-11 23:34:41 -0400

    fix indentions

--- components/post/PostContainer.js
+++ components/post/PostContainer.js
@@ -6,29 +6,29 @@ const PostContainer = props => (
     {props.children}
     <style jsx>{`
       .container {
-      margin: 0 auto;
-      width: 95%;
-      padding: 30px 0;
-    }
-    @media (min-width: 768px) {
-      .container {
-        max-width: 750px;
-        width: auto;
+        margin: 0 auto;
+        width: 95%;
+        padding: 30px 0;
       }
-    }
-    @media (min-width: 992px) {
-      .container {
-        max-width: 970px;
-        width: auto;
+      @media (min-width: 768px) {
+        .container {
+          max-width: 750px;
+          width: auto;
+        }
       }
-    }
-    @media (min-width: 1200px) {
-      .container {
-        max-width: 1170px;
-        width: auto;
+      @media (min-width: 992px) {
+        .container {
+          max-width: 970px;
+          width: auto;
+        }
       }
-    }
-  `}</style>
+      @media (min-width: 1200px) {
+        .container {
+          max-width: 1170px;
+          width: auto;
+        }
+      }
+    `}</style>
   </div>
 );
 
