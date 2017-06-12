562ce0c47b1587c7e569db28bf886f57d624f72d
Logan McAnsh <logan@mcan.sh>
2017-06-11 23:34:58 -0400

    update banners

--- pages/_posts.js
+++ pages/_posts.js
@@ -4,41 +4,41 @@ function Posts() {
       link: 'sinatra-project',
       title: 'Sinatra Project',
       date: 'April 25, 2017',
-      image_url: 'wesson-wang-110739.jpg'
+      image_url: 'farzad-nazifi-71686.jpg'
     },
     {
       link: 'time-to-hex',
       title: 'How To Convert The Current Time To A Hexadecimal',
       date: 'January 10, 2017',
-      image_url: 'wesson-wang-110739.jpg'
+      image_url: 'team-ui8-199275.jpg'
     },
     {
       link: 'similarities-between-life-and-code',
       title: 'Similarities Between Life And Code',
       date: 'December 25, 2016',
-      image_url: 'wesson-wang-110739.jpg'
+      image_url: 'markus-spiske-109588.jpg'
     },
     {
       link: 'rack-key-concepts',
       title: 'Rack Key Concepts',
       date: 'December 7, 2016',
-      image_url: 'wesson-wang-110739.jpg'
+      image_url: 'michail-sapiton-234263.jpg'
     },
     {
       link: 'javascript-classes-key-concepts',
       title: 'Javascript Classes Key Concepts',
       date: 'November 21, 2016',
-      image_url: 'wesson-wang-110739.jpg'
+      image_url: 'markus-spiske-109588.jpg'
     },
     {
       link: 'object-lifecycle-cheatsheet',
       title: 'Object Lifecycle Cheatsheet',
       date: 'November 4, 2016',
-      image_url: 'wesson-wang-110739.jpg'
+      image_url: 'farzad-nazifi-71686.jpg'
     },
     {
       link: 'html5-progress-element',
-      title: 'The Progress Element is great for blogs',
+      title: 'The HTML5 Progress Element is great for blogs',
       date: 'October 14, 2016',
       image_url: 'wesson-wang-110739.jpg'
     },
@@ -46,7 +46,7 @@ function Posts() {
       link: 'why-im-learning-software-development',
       title: "Why I'm Learning Software Development",
       date: 'October 3, 2016',
-      image_url: 'wesson-wang-110739.jpg'
+      image_url: 'team-ui8-199275.jpg'
     },
   ];
 }
