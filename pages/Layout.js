f9b1f660baa5f3a659666d400640c2aa9aece3b8
Logan McAnsh <logan@mcan.sh>
2017-06-11 23:34:49 -0400

    update layout

--- pages/Layout.js
+++ pages/Layout.js
@@ -4,13 +4,13 @@ import Head from 'next/head';
 import Normalize from '../components/css/Normalize';
 import BodyStyles from '../components/css/BodyStyles';
 
-const Document = ({ children }) => (
+const Layout = ({ children }) => (
   <div>
     <Head>
       <title>Logan McAnsh</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="mask-icon" href="/static/images/website_icon.svg" color="#FC6C2B" />
+      <link rel="mask-icon" href="/static/images/website_icon.svg" color="#E53A40" />
       <link rel="shortcut icon" href="/static/images/favicon.png" />
       <Normalize />
       <BodyStyles />
@@ -20,8 +20,8 @@ const Document = ({ children }) => (
   </div>
 );
 
-Document.propTypes = {
+Layout.propTypes = {
   children: PropTypes.node.isRequired
 };
 
-export default Document;
+export default Layout;
