fb8f22bce5f34835dbe9550bf5b00694618e8871
Logan McAnsh <logan@mcan.sh>
2017-06-15 16:43:46 -0400

    update dependencies

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import flush from 'styled-jsx/server';
 import Document, { Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   static async getInitialProps({ renderPage }) {
     const page = renderPage();
-    const styles = flush();
-    return { ...page, styles };
+    return { ...page };
   }
 
   render() {
@@ -18,8 +16,8 @@ export default class MyDocument extends Document {
           <link rel="mask-icon" href="/static/images/website_icon.svg" color="#E53A40" />
           <link rel="shortcut icon" href="/static/images/favicon.png" />
           <link rel="stylesheet" href="/static/css/style.css" />
-          <link href="/atom" type="application/atom+xml" rel="alternate" title="RSS Feed" />
-          <link href="/feed.json" type="application/json" rel="alternate" title="JSON Feed" />
+          <link rel="alternate" href="/atom" type="application/atom+xml" title="RSS Feed" />
+          <link rel="alternate" href="/feed.json" type="application/json" title="JSON Feed" />
         </Head>
         <body>
           <Main />
