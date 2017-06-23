a61d800803a2916b440dfe3ab799face6074c6dd
Logan McAnsh <logan@mcan.sh>
2017-06-23 01:38:58 -0400

    :rocket: headerBlur function is now apart of the header component!

--- components/Header.js
+++ components/Header.js
@@ -38,87 +38,113 @@ function buttonLink(link) {
   );
 }
 
-const Header = (props) => {
-  const Date = props.date ? <h2>{props.date}</h2> : '';
-  return (
-    <header>
-      <div className="header__bg" style={{ backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.74902), rgba(0, 0, 0, 0.74902)), url(/static/images/${props.image})` }} />
-      <div className="header__content">
-        <h1>{props.text}</h1>
-        {buttonLink(props.link)}
-        {Date}
-      </div>
-      <style jsx>{`
-        header {
-          height: 50vh;
-          min-height: 500px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          position: relative;
-          font-size: 1.7em;
-          text-align: center;
-        }
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { blur: 0 };
+    this.handleScroll = this.handleScroll.bind(this);
+  }
 
-        .header__bg {
-          position: absolute;
-          top: 0;
-          right: 0;
-          bottom: 0;
-          left: 0;
-          background-position: center;
-          background-size: cover;
-        }
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll, true);
+  }
 
-        .header__content {
-          color: white;
-          z-index: 1;
-          height: 100%;
-          width: 100%;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll, true);
+  }
 
-        @supports (backdrop-filter: initial) and (-webkit-backdrop-filter: initial) {
+  handleScroll() {
+    const scroll = window.pageYOffset;
+    const maxMinBlur = Math.min(100, Math.max(0, scroll / 3));
+    const blur = `blur(${maxMinBlur}px)`;
+    this.setState({ blur });
+  }
+  render() {
+    const Date = this.props.date ? <h2>{this.props.date}</h2> : '';
+    const blurStyles = {
+      backdropFilter: `${this.state.blur}`,
+      WebkitBackdropFilter: `${this.state.blur}`
+    };
+    return (
+      <header>
+        <div className="header__bg" style={{ backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.74902), rgba(0, 0, 0, 0.74902)), url(/static/images/${this.props.image})` }} />
+        <div className="header__content" style={blurStyles}>
+          <h1>{this.props.text}</h1>
+          {buttonLink(this.props.link)}
+          {Date}
+        </div>
+        <style jsx>{`
+          header {
+            height: 50vh;
+            min-height: 500px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+            font-size: 1.7em;
+            text-align: center;
+          }
+
+          .header__bg {
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            background-position: center;
+            background-size: cover;
+          }
+
           .header__content {
-            will-change: backdrop-filter;
-            backdrop-filter: blur(0px);
-            -webkit-backdrop-filter: blur(0px);
+            color: white;
+            z-index: 1;
+            height: 100%;
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
           }
-        }
 
-        h1, h2 {
-          max-width: 80%;
-        }
+          @supports (backdrop-filter: initial) and (-webkit-backdrop-filter: initial) {
+            .header__content {
+              will-change: backdrop-filter;
+              backdrop-filter: blur(0px);
+              -webkit-backdrop-filter: blur(0px);
+            }
+          }
 
-        @media (max-width: 410px) {
-          h1,h2 {
-            max-width: 90%;
+          h1, h2 {
+            max-width: 80%;
           }
-          h1 {
-            font-size: 1.5em;
-          }
-        }
 
-        @media (max-height: 500px) {
-          header {
-            height: 100vh;
-            min-height: 100vh;
+          @media (max-width: 410px) {
+            h1,h2 {
+              max-width: 90%;
+            }
+            h1 {
+              font-size: 1.5em;
+            }
           }
-        }
 
-        @media (max-width: 500px) {
-          header {
-            min-height: 95vh;
-            height: 95vh;
+          @media (max-height: 500px) {
+            header {
+              height: 100vh;
+              min-height: 100vh;
+            }
           }
-        }
-      `}</style>
-    </header>
-  );
-};
+
+          @media (max-width: 500px) {
+            header {
+              min-height: 95vh;
+              height: 95vh;
+            }
+          }
+        `}</style>
+      </header>
+    );
+  }
+}
 
 Header.defaultProps = {
   date: '',
@@ -128,6 +154,9 @@ Header.defaultProps = {
 Header.propTypes = {
   text: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
+  date: PropTypes.string,
+  link: PropTypes.string
 };
+
 
 export default Header;
--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,3 @@
-function headerBlur() {
-  const header = document.querySelector('.header__content');
-  if (!header) return;
-  const scroll = window.pageYOffset;
-  const maxMinBlur = Math.min(100, Math.max(0, scroll / 3));
-  const blur = `blur(${maxMinBlur}px`;
-  header.style.backdropFilter = blur;
-  header.style.webkitBackdropFilter = blur;
-}
-
-window.addEventListener('scroll', headerBlur);
-
 function youReadThisMuch() {
   const progress = document.querySelector('progress');
   if (!progress) return;
