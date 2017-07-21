8eda11bdeabe6a991be20709429405c94a930dc2
Logan McAnsh <logan@mcan.sh>
2017-07-20 21:54:27 -0400

    service worker is being built but chrome thinks it doesnt exist

--- next.config.js
+++ next.config.js
@@ -1,11 +1,44 @@
 const webpack = require('webpack');
 const path = require('path');
 const glob = require('glob');
-
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 require('dotenv').config({ path: 'variables.env' });
 
 module.exports = {
-  webpack: (config) => {
+  webpack: (config, { dev }) => {
+    const oldEntry = config.entry;
+    config.entry = () => {
+      oldEntry().then(entry => {
+        entry['main.js'].push(path.resolve('./utils/offline'));
+        return entry;
+      });
+    }
+    /* Enable only in Production */
+    if (!dev) {
+      // Service Worker
+      config.plugins.push(
+        new SWPrecacheWebpackPlugin({
+          cacheId: 'mcansh.blog',
+          filename: 'sw.js',
+          minify: true,
+          staticFileGlobsIgnorePatterns: [/\.next\//],
+          staticFileGlobs: [
+            'static/**/*' // Precache all static files by default
+          ],
+          runtimeCaching: [
+            // Example with different handlers
+            {
+              handler: 'fastest',
+              urlPattern: /[.](png|jpg|css)/
+            },
+            {
+              handler: 'networkFirst',
+              urlPattern: /^http.*/ //cache all files
+            }
+          ]
+        })
+      );
+    }
     config.plugins.push(
       new webpack.DefinePlugin({
         'process.env.GITHUB': JSON.stringify(process.env.GITHUB),
@@ -26,8 +59,10 @@ module.exports = {
       },
       {
         test: /\.sass$/,
-        use: ['babel-loader', 'raw-loader', 'postcss-loader',
-          { loader: 'sass-loader',
+        use: [
+          'babel-loader', 'raw-loader', 'postcss-loader',
+          {
+            loader: 'sass-loader',
             options: {
               includePaths: ['styles', 'node_modules']
                 .map(d => path.join(__dirname, d))
