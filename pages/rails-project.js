904133158d5f996cbdd0b337d339a7d2dc1ea74e
Logan McAnsh <logan@mcan.sh>
2017-08-07 10:42:58 -0400

    update rails post, need to write it now

--- pages/rails-project.js
+++ pages/rails-project.js
@@ -1,34 +1,27 @@
 import React from 'react';
-import Link from 'next/link';
+// import Link from 'next/link';
 import Head from 'next/head';
 import Document from '../layouts/Document';
 import Progress from '../components/post/Progress';
 import Header from '../components/Header';
 import PostContainer from '../components/post/PostContainer';
+import { H1 } from '../components/post/Heading';
 import P from '../components/post/Paragraph';
 
 const Index = () => {
   const title = 'Rails Project';
-  const image = 'alesia-kazantceva-283288.jpg';
+  const image = '1*Wmv8hfi_bTHuHyV5CawnCw.jpg';
   return (
     <Document>
       <Head>
         <title>{title}</title>
         <meta name="twitter:title" content={title} />
-        <meta name="twitter:image" content={image} />
+        <meta name="twitter:image" content={`/static/images/${image}`} />
       </Head>
-      <Header text={title} image={image} date="TBD" />
+      <Header text={title} image={image} date="August 10, 2017" />
       <PostContainer>
         <Progress />
-
-        <P>HEY!</P>
-
-        <style jsx>{`
-          a {
-            color: #E53A40;
-            text-decoration-skip: ink;
-          }
-        `}</style>
+        <H1>Oh Rails....</H1>
       </PostContainer>
     </Document>
   );
