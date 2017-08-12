03de7b2ebbb0f3ff97332190e9f4019c68c75b40
Logan McAnsh <logan@mcan.sh>
2017-08-12 18:44:57 -0400

    Delete postcss.config.js

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-const postcss = require('postcss-easy-import');
-const autoprefixer = require('autoprefixer');
-
-module.exports = {
-  plugins: [
-    postcss({ prefix: '_' }), // keep this first
-    autoprefixer({ /* ...options */ }), // so imports are auto-prefixed too
-  ],
-};
