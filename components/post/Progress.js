4771a85217db12169bdb6363c8ba40ba92524703
Logan McAnsh <logan@mcan.sh>
2017-08-17 13:59:58 -0400

    remove old progress bar color

--- components/post/Progress.js
+++ components/post/Progress.js
@@ -32,12 +32,10 @@ class Progress extends React.Component {
           }
 
           progress::-webkit-progress-value {
-            /*background-color: #E53A40;*/
             background-image: linear-gradient(135deg, #52E5E7 0%, #130CB7 100%);
           }
 
           progress::-moz-progress-bar {
-            /*background-color: #E53A40;*/
             background-image: linear-gradient(135deg, #52E5E7 0%, #130CB7 100%);
           }
 
