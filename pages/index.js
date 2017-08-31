debb9a48317fbf4b1c6c899f48f20e3cc07ca4b3
Logan McAnsh <logan@mcan.sh>
2017-08-31 18:24:19 -0400

    eslint

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,11 @@ class Index extends React.Component {
       navigator.serviceWorker
         .register('/sw.js')
         .then((registration) => {
+          // eslint-disable-next-line no-console
           console.log('service worker registration successful', registration.active);
         })
         .catch((err) => {
+          // eslint-disable-next-line no-console
           console.warn('service worker registration failed', err.message);
         });
     }
