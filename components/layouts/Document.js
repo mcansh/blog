6cb2b892640b09a07d9142576ec2b02c0b3e4a83
Logan McAnsh <logan@mcan.sh>
2017-11-27 22:36:50 -0500

    readd nprogress
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- components/layouts/Document.js
+++ components/layouts/Document.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import PropTypes from 'prop-types';
 import colors from '../../theme';
 import Footer from '../Footer';
 import { version } from '../../package.json';
+
+Router.onRouteChangeStart = () => NProgress.start();
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
 
 if (global.document) {
   const info = [
