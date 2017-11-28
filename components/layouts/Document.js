a3031ffc677374201b3cb49b87ad62ca275cc315
Logan McAnsh <logan@mcan.sh>
2017-11-27 22:38:09 -0500

    register service worker and raven when component mounts

--- components/layouts/Document.js
+++ components/layouts/Document.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Raven from 'raven-js';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import PropTypes from 'prop-types';
@@ -20,65 +21,86 @@ if (global.document) {
   info.forEach(message => console.log(message));
 }
 
-const Document = ({ children }) => (
-  <div>
-    {children}
-    <Footer />
-    <style jsx global>{`
-      * {
-        box-sizing: border-box;
-        margin: 0;
+class Document extends Component {
+  componentDidMount() {
+    if (process.env.NODE_ENV === 'production') {
+      Raven.config(process.env.SENTRY, {
+        release: version,
+        environment: process.env.NODE_ENV,
+      }).install();
+
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+          .register('/service-worker.js')
+          .then(console.log('service worker registration successful')) // eslint-disable-line no-console
+          .catch(err => console.warn(err)); // eslint-disable-line no-console
       }
-      html {
-        font-size: 10px;
-      }
-      body {
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica,
-          Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
-          'Segoe UI Symbol';
-        font-weight: 400;
-        margin: 0;
-      }
-      ::selection {
-        background: ${colors.primary};
-        color: white;
-      }
-      a {
-        color: ${colors.primary};
-        text-decoration-skip: ink;
-        transition: 300ms all ease-in-out;
-      }
-      a:hover {
-        color: ${colors.secondary};
-      }
-      a::selection {
-        color: white;
-      }
-      #nprogress {
-        pointer-events: none;
-      }
-      #nprogress .bar {
-        background-image: ${colors.gradient};
-        position: fixed;
-        z-index: 1031;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 2px;
-      }
-      #nprogress .peg {
-        display: block;
-        position: absolute;
-        right: 0px;
-        width: 100px;
-        height: 100%;
-        box-shadow: 0 0 10px #ff9300, 0 0 5px #ff9300;
-        opacity: 1;
-        transform: rotate(3deg) translate(0px, -4px);
-      }
-    `}</style>
-  </div>
-);
+    }
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <div>
+        {children}
+        <Footer />
+        <style jsx global>{`
+          * {
+            box-sizing: border-box;
+            margin: 0;
+          }
+          html {
+            font-size: 10px;
+          }
+          body {
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI',
+              Helvetica, Arial, sans-serif, 'Apple Color Emoji',
+              'Segoe UI Emoji', 'Segoe UI Symbol';
+            font-weight: 400;
+            margin: 0;
+          }
+          ::selection {
+            background: ${colors.primary};
+            color: white;
+          }
+          a {
+            color: ${colors.primary};
+            text-decoration-skip: ink;
+            transition: 300ms all ease-in-out;
+          }
+          a:hover {
+            color: ${colors.secondary};
+          }
+          a::selection {
+            color: white;
+          }
+          #nprogress {
+            pointer-events: none;
+          }
+          #nprogress .bar {
+            background-image: ${colors.primary};
+            position: fixed;
+            z-index: 1031;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 2px;
+          }
+          #nprogress .peg {
+            display: block;
+            position: absolute;
+            right: 0px;
+            width: 100px;
+            height: 100%;
+            box-shadow: 0 0 10px #ff9300, 0 0 5px #ff9300;
+            opacity: 1;
+            transform: rotate(3deg) translate(0px, -4px);
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
 
 Document.propTypes = { children: PropTypes.node.isRequired };
 
