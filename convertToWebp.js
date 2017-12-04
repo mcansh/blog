dac65b0af756b8f827a2b69fa45af928b2d563ec
Logan McAnsh <logan@mcan.sh>
2017-12-04 15:27:41 -0500

    update convertToWebp to filter out directories and non images
    (cherry picked from commit 7bf15754c4ba06c2f9e92e184f182fd7bd8a66a0)

--- convertToWebp.js
+++ convertToWebp.js
@@ -1,18 +1,26 @@
 const webp = require('webp-converter'); // eslint-disable-line import/no-extraneous-dependencies
+const readChunk = require('read-chunk'); // eslint-disable-line import/no-extraneous-dependencies
+const fileType = require('file-type'); // eslint-disable-line import/no-extraneous-dependencies
 const fs = require('fs');
-const path = require('path');
 
 const imagesFolder = './static/images/posts';
 
 fs.readdirSync(imagesFolder).forEach(file => {
-  const extension = path.extname(file);
-  const fileNoExtension = file.replace(extension, '');
-  webp.cwebp(
-    `${imagesFolder}/${file}`,
-    `${imagesFolder}/webp/${fileNoExtension}.webp`,
-    '-q 80',
-    status => {
-      console.log(status); // eslint-disable-line no-console
-    },
-  );
+  const stats = fs.lstatSync(`${imagesFolder}/${file}`);
+  const isDirectory = stats.isDirectory();
+  if (!isDirectory) {
+    const buffer = readChunk.sync(`${imagesFolder}/${file}`, 0, 4100);
+    const type = fileType(buffer);
+    if (type && type.mime.startsWith('image/')) {
+      const fileNoExtension = file.replace(type.ext, '');
+      webp.cwebp(
+        `${imagesFolder}/${file}`,
+        `${imagesFolder}/webp/${fileNoExtension}webp`,
+        '-q 80',
+        status => {
+          console.log(status); // eslint-disable-line no-console
+        },
+      );
+    }
+  }
 });
