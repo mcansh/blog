a082c9cebeaafe662920f1b13d139abb0d8c087a
Logan McAnsh <logan@mcan.sh>
2018-01-13 10:39:48 -0500

    make NProgress show up when changing routes

--- components/layouts/Document.js
+++ components/layouts/Document.js
@@ -7,6 +7,7 @@ import colors from '../../theme';
 import Footer from '../Footer';
 import { version } from '../../package.json';
 
+NProgress.configure({ showSpinner: false });
 Router.onRouteChangeStart = () => NProgress.start();
 Router.onRouteChangeComplete = () => NProgress.done();
 Router.onRouteChangeError = () => NProgress.done();
@@ -49,9 +50,11 @@ class Document extends Component {
             box-sizing: border-box;
             margin: 0;
           }
+
           html {
             font-size: 10px;
           }
+
           body {
             font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI',
               Helvetica, Arial, sans-serif, 'Apple Color Emoji',
@@ -59,26 +62,48 @@ class Document extends Component {
             font-weight: 400;
             margin: 0;
           }
+
           ::selection {
             background: ${colors.primary};
             color: white;
           }
+
           a {
             color: ${colors.primary};
             text-decoration-skip: ink;
             transition: 300ms all ease-in-out;
           }
+
           a:hover {
             color: ${colors.secondary};
           }
+
           a::selection {
             color: white;
           }
+
           #nprogress {
             pointer-events: none;
           }
+
           #nprogress .bar {
-            background-image: ${colors.primary};
+            background: ${colors.primary};
+
+            position: fixed;
+            z-index: 1031;
+            top: 0;
+            left: 0;
+
+            width: 100%;
+            height: 2px;
+          }
+
+          #nprogress {
+            pointer-events: none;
+          }
+
+          #nprogress .bar {
+            background: ${colors.primary};
             position: fixed;
             z-index: 1031;
             top: 0;
@@ -86,15 +111,26 @@ class Document extends Component {
             width: 100%;
             height: 2px;
           }
+
           #nprogress .peg {
             display: block;
             position: absolute;
             right: 0px;
             width: 100px;
             height: 100%;
-            box-shadow: 0 0 10px #ff9300, 0 0 5px #ff9300;
+            box-shadow: 0 0 10px ${colors.primary}, 0 0 5px ${colors.primary};
             opacity: 1;
             transform: rotate(3deg) translate(0px, -4px);
+          }
+
+          .nprogress-custom-parent {
+            overflow: hidden;
+            position: relative;
+          }
+
+          .nprogress-custom-parent #nprogress .spinner,
+          .nprogress-custom-parent #nprogress .bar {
+            position: absolute;
           }
         `}</style>
       </div>
