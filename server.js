4b009f4136aa36a4fcfa6fc09f92dc46d7d6276d
Logan McAnsh <logan@mcan.sh>
2018-02-01 20:56:52 -0500

    http2 server with polka

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import polka from 'polka';
+import { createServer } from 'spdy';
+import { readFileSync } from 'fs';
+
 import { parse } from 'url';
 import next from 'next';
 import { join } from 'path';
@@ -7,39 +10,40 @@ import atom from './lib/atom';
 import jsonfeed from './lib/jsonfeed';
 import manifest from './lib/manifest';
 
+const options = {
+  key: readFileSync('./server.key'),
+  cert: readFileSync('./server.crt'),
+};
+
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const PORT = process.env.PORT || 3000;
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = polka();
+  const { handler } = polka()
+    .get('/service-worker.js', (req, res) => {
+      const parsedUrl = parse(req.url, true);
+      const { pathname } = parsedUrl;
+      const filePath = join(__dirname, '.next', pathname);
+      app.serveStatic(req, res, filePath);
+    })
+    .get('/manifest.json', (req, res) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(manifest());
+    })
+    .get('/feed.json', (req, res) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(jsonfeed());
+    })
+    .get('/atom', (req, res) => {
+      res.setHeader('Content-Type', 'text/xml');
+      res.end(atom());
+    })
+    .get('*', (req, res) => handle(req, res));
 
-  server.get('/service-worker.js', (req, res) => {
-    const parsedUrl = parse(req.url, true);
-    const { pathname } = parsedUrl;
-    const filePath = join(__dirname, '.next', pathname);
-    app.serveStatic(req, res, filePath);
+  createServer(options, handler).listen(3000, err => {
+    if (err) throw new Error(err);
+    console.log(`> Ready on http://localhost:${PORT}`); // eslint-disable-line no-console
   });
-
-  server.get('/manifest.json', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(manifest());
-  });
-
-  server.get('/atom', (req, res) => {
-    res.setHeader('Content-Type', 'text/xml');
-    res.end(atom());
-  });
-
-  server.get('/feed.json', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(jsonfeed());
-  });
-
-  server.get('*', (req, res) => handle(req, res));
-
-  server
-    .listen(PORT)
-    .then(() => console.log(`> Ready on http://localhost:${PORT}`)); // eslint-disable-line no-console
 });
