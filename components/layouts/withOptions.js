84985c5dc9f3b91732cef4bb98acd0c2fc8b16fe
Logan McAnsh <logan@mcan.sh>
2018-02-16 14:52:57 -0500

    named export

--- components/layouts/withOptions.js
+++ components/layouts/withOptions.js
@@ -3,10 +3,12 @@ import Post from './Post';
 import Meta from '../Meta';
 import Header from '../Header';
 
-export default options => content => () => (
-  <Post>
+const withOptions = options => content => () => (
+<Post>
     <Meta {...options} />
     <Header {...options} />
     {content}
   </Post>
-);
+)
+
+export default withOptions
