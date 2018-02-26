4064fe56a0981b8d5b7147378d31111556f0e685
Logan McAnsh <logan@mcan.sh>
2018-02-26 15:40:17 -0500

    use custom eslint config
    also updates webpack
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,9 @@
 module.exports = {
-  extends: ['airbnb', 'prettier', 'prettier/react'],
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  parser: 'babel-eslint',
-  plugins: ['react', 'jsx-a11y', 'import', 'prettier'],
+  extends: ['mcansh'],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'jsx-a11y/href-no-hash': 0,
-    'react/jsx-closing-tag-location': 0,
-    'react/jsx-curly-brace-presence': 0,
     'jsx-a11y/anchor-is-valid': 0,
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/no-noninteractive-element-interactions': 0,
-    'jsx-a11y/label-has-for': 0,
-    'no-param-reassign': [2, { props: false }],
-    'prettier/prettier': [
-      'error',
-      {
-        trailingComma: 'es5',
-        singleQuote: true
-      }
-    ]
   }
 };
