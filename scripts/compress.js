5cf2b9c160a9993920a4514bbc8cfb394ad6eaca
Logan McAnsh <logan@mcan.sh>
2018-03-07 21:05:09 -0500

    generate thumb nail images

--- scripts/compress.js
+++ scripts/compress.js
@@ -3,7 +3,27 @@ const imagemin = require('imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageminPngquant = require('imagemin-pngquant');
 const imageminWebp = require('imagemin-webp');
+const sharp = require('sharp');
+// const { lookup } = require('mime-types');
+const { readdirSync } = require('fs');
 /* eslint-enable import/no-extraneous-dependencies */
+
+const imageDir = 'static/images/posts';
+readdirSync(imageDir).forEach(file => {
+  const fullFile = `${imageDir}/${file}`;
+  // const fileType = lookup(fullFile);
+  const fileExtension = /[^.]+$/.exec(file);
+  const fileName = file.replace(`.${fileExtension}`, '');
+  // const reg = /image\/jpeg|image\/png/;
+  // if (reg.test(fileType)) {
+  sharp(fullFile)
+    .resize(150)
+    .toFile(`${imageDir}/thumb/${fileName}.${fileExtension}`, (err, info) => {
+      console.log('err: ', err);
+      console.log('info: ', info);
+    });
+  // }
+});
 
 // compress and convert post images to webp
 imagemin(['static/images/posts/*.{jpg,png}'], 'static/images/posts', {
