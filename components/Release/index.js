fb6886786f6beb2f27a857d9507554a21ae8ffc4
Logan McAnsh <logan@mcan.sh>
2018-03-16 16:30:22 -0400

    Changelog Updates (#159)
    * update withData with changes from https://github.com/zeit/next.js/pull/3973
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * arrow function 😉
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * changelog updates
    
    - update font sizes
    - remove pre-release tag
    - move to Releases subfolder
    
    * @std/esm is now just esm
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * chore(package): update dependencies
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- components/Release/index.js
+++ components/Release/index.js
@@ -6,56 +6,17 @@ import github from 'remark-github';
 import emoji from 'remark-emoji';
 import reactRenderer from 'remark-react';
 import { injectIntl, intlShape } from 'react-intl';
-import styled from 'styled-components';
+import { H1, H3 } from './heading';
+import ReleaseWrap from './wrap';
+import List from './list';
+import ListItem from './item';
 
-const ReleaseWrap = styled.div`
-  margin: 2rem 4rem;
-`;
-
-const List = styled.ul`
-  padding: 0;
-  list-style-type: disc;
-  list-style-position: inside;
-`;
-
-const ListItem = styled.li`
-  font-size: 1.2rem;
-`;
-
-const H3 = styled.h3`
-  margin: 1rem 0;
-`;
-
-const H1 = styled.h1`
-  margin-bottom: 1rem;
-  display: flex;
-`;
-
-const PreRelease = styled.span`
-  border: 1px solid #b71c1c;
-  font-size: 1.2rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  line-height: 1;
-  padding: 0.5rem 1rem;
-  border-radius: 1rem;
-  margin-left: 2rem;
-`;
-
-const Release = ({
-  version,
-  notes,
-  date,
-  isPrerelease,
-  intl: { formatRelative },
-}) => (
+const Release = ({ version, notes, date, intl: { formatRelative } }) => (
   <ReleaseWrap>
     <H1>
       <Link href={`https://github.com/mcansh/blog/releases/${version}`}>
         <a>Version {version}</a>
       </Link>
-      {isPrerelease && <PreRelease>Pre-Release</PreRelease>}
     </H1>
     <h2>{formatRelative(date)}</h2>
     {
@@ -80,7 +41,6 @@ Release.propTypes = {
   version: PropTypes.string.isRequired,
   notes: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
-  isPrerelease: PropTypes.bool.isRequired,
   intl: intlShape.isRequired,
 };
 
---
+++ components/Release/wrap.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const ReleaseWrap = styled.div`
+  margin: 2rem 4rem;
+`;
+
+export default ReleaseWrap;
