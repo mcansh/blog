b0f4ae8f6a2cb48e91891aefe96592971d74adbc
Logan McAnsh <logan@mcan.sh>
2018-03-31 15:50:56 -0400

    update eslintrc to use 'off/warning/error' instead of magic numbers

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
   extends: ['mcansh'],
   rules: {
-    'jsx-a11y/href-no-hash': 0,
-    'jsx-a11y/anchor-is-valid': 0,
-    'jsx-a11y/click-events-have-key-events': 0,
-    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/href-no-hash': "off",
+    'jsx-a11y/anchor-is-valid': "off",
+    'jsx-a11y/click-events-have-key-events': "off",
+    'jsx-a11y/no-noninteractive-element-interactions': "off",
+    'no-return-assign': ["error", "except-parens"]
   }
 };
