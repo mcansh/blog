0725338d92c5e0601ac38a45101e870ba48e84b2
Logan McAnsh <logan@mcan.sh>
2018-04-17 22:10:24 -0400

    :white_check_mark: postcard tests
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ __tests__/PostCard.test.js
@@ -0,0 +1,27 @@
+/* eslint-env jest */
+import { shallow } from 'enzyme';
+import React from 'react';
+
+import PostCard, { Title } from '../components/PostCard';
+import posts from '../posts.json';
+
+describe('PostCard Component', () => {
+  it('shows image, title, and date', () => {
+    const [latest] = posts;
+    const post = shallow(
+      <PostCard
+        key={latest.id}
+        image={latest.image}
+        date={latest.date}
+        title={latest.title}
+        id={latest.id}
+      />
+    );
+    expect(
+      post
+        .find(Title)
+        .render()
+        .text()
+    ).toBe(latest.title);
+  });
+});
