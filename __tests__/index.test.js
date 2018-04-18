e2f1c4860bc8cd77b7346ff5ac73203429bda076
Logan McAnsh <logan@mcan.sh>
2018-04-17 22:09:40 -0400

    :white_check_mark: verify all the posts render
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,12 +5,21 @@ import React from 'react';
 
 import App from '../pages/index';
 import Header from '../components/Header';
+import PostCard from '../components/PostCard';
+
+import posts from '../posts.json';
 
 describe('With Enzyme', () => {
   it('App renders', () => {
     const app = shallow(<App />);
 
     expect(app.find(Header)).toHaveLength(1);
+  });
+
+  it('shows all the posts', () => {
+    const app = shallow(<App />);
+
+    expect(app.find(PostCard)).toHaveLength(posts.length);
   });
 });
 
