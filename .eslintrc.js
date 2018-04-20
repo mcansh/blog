83f2ce46e48c49259ebe73d16c72ca5bdd3e03cd
Logan McAnsh <logan@mcan.sh>
2018-04-20 09:13:07 -0400

    move test suite to devDependencies, add eslint rule for test
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,14 @@ module.exports = {
   extends: ['plugin:jest/recommended', 'mcansh'],
   plugins: ['jest'],
   rules: {
-    'jsx-a11y/href-no-hash': "off",
-    'jsx-a11y/anchor-is-valid': "off",
-    'jsx-a11y/click-events-have-key-events': "off",
-    'jsx-a11y/no-noninteractive-element-interactions': "off",
-    'no-return-assign': ["error", "except-parens"]
+    'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'no-return-assign': ['error', 'except-parens'],
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['**/*.test.js', 'jest.setup.js', 'jest.config.js'] },
+    ],
   }
 };
