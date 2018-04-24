90fa4dfb37b556b2c121a8ed6a0ec55fbbe18b8e
Logan McAnsh <logan@mcan.sh>
2018-04-23 21:00:46 -0400

    swap formatRelative injectted API for component
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- components/Release/index.js
+++ components/Release/index.js
@@ -5,20 +5,22 @@ import remark from 'remark';
 import github from 'remark-github';
 import emoji from 'remark-emoji';
 import reactRenderer from 'remark-react';
-import { injectIntl, intlShape } from 'react-intl';
+import { FormattedRelative } from 'react-intl';
 import { H1, H3 } from './heading';
 import ReleaseWrap from './wrap';
 import List from './list';
 import ListItem from './item';
 
-const Release = ({ version, notes, date, intl: { formatRelative } }) => (
+const Release = ({ version, notes, date }) => (
   <ReleaseWrap>
     <H1>
       <Link href={`https://github.com/mcansh/blog/releases/${version}`}>
         <a>Version {version}</a>
       </Link>
     </H1>
-    <h2>{formatRelative(date)}</h2>
+    <h2>
+      <FormattedRelative value={date} />
+    </h2>
     {
       remark()
         .use(reactRenderer, {
@@ -41,7 +43,6 @@ Release.propTypes = {
   version: PropTypes.string.isRequired,
   notes: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
-  intl: intlShape.isRequired,
 };
 
-export default injectIntl(Release);
+export default Release;
