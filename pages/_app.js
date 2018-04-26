1fe4ab3bb21b63c385dcc5ea66f3dd842c6e7b92
Logan McAnsh <logan@mcan.sh>
2018-04-26 17:27:26 -0400

    update _app.js to properly use sentry for error tracking

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,14 @@ import Raven from 'raven';
 import { version } from '../package.json';
 
 export default class MyApp extends App {
-  componentDidCatch(error, errorInfo) {
+  constructor(props) {
+    super(props);
     Raven.config(process.env.SENTRY, {
       release: version,
       environment: process.env.NODE_ENV || 'development',
     }).install();
+  }
+  componentDidCatch(error, errorInfo) {
     super.componentDidCatch(error, errorInfo);
     Raven.captureException(error, { extra: errorInfo });
   }
