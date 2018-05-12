82d760d2ea87c6cecebd6d38c8d09e5224ca8950
Logan McAnsh <logan@mcan.sh>
2018-05-12 01:51:06 -0400

    lazy load more releases (minor) closes #216
    currently onClick as im looking for good ways to do it based on scroll
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/changelog.js
+++ pages/changelog.js
@@ -1,22 +1,49 @@
 import React, { Fragment } from 'react';
+import styled from 'styled-components';
 import { gql } from 'apollo-boost';
 import { Query } from 'react-apollo';
+import { lighten } from 'polished';
 import withData from '../lib/withData';
 import Meta from '../components/Meta';
 import Header from '../components/Header';
 import Release from '../components/Release';
 
+const Button = styled.button`
+  all: unset;
+  border: none;
+  color: white;
+  padding: 1rem 2rem;
+  border-radius: 1.2rem;
+  background: ${props => props.theme.primary};
+  margin: 2rem auto;
+  display: block;
+  font-size: 1.6rem;
+  transition: 250ms all ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    background: ${props => lighten(0.2, props.theme.primary)};
+  }
+`;
+
 const allReleasesQuery = gql`
-  query allReleases {
+  query allReleases($after: String) {
     repository(owner: "mcansh", name: "blog") {
       name
-      releases(first: 100, orderBy: { field: CREATED_AT, direction: DESC }) {
-        nodes {
-          tag {
-            name
+      releases(
+        first: 10
+        after: $after
+        orderBy: { field: CREATED_AT, direction: DESC }
+      ) {
+        edges {
+          cursor
+          node {
+            tag {
+              name
+            }
+            description
+            publishedAt
           }
-          description
-          publishedAt
         }
       }
     }
@@ -35,22 +62,54 @@ const Changelog = () => (
       }}
     />
     <Query query={allReleasesQuery}>
-      {({ loading, error, data }) => {
+      {({ loading, error, fetchMore, data }) => {
         if (loading) return <p>Loading...</p>;
         if (error) return <p>Error :(</p>;
         const {
           repository: {
-            releases: { nodes },
+            releases: { edges: releases },
           },
         } = data;
-        return nodes.map(release => (
-          <Release
-            key={release.tag.name}
-            version={release.tag.name}
-            notes={release.description}
-            date={release.publishedAt}
-          />
-        ));
+
+        const loadMoreReleases = () => {
+          const lastRelease = releases[releases.length - 1];
+
+          fetchMore({
+            variables: {
+              after: lastRelease.cursor,
+            },
+            updateQuery: (prev, { fetchMoreResult }) => {
+              if (!fetchMoreResult) return prev;
+
+              return {
+                repository: {
+                  ...prev.repository,
+                  releases: {
+                    ...prev.repository.releases,
+                    edges: [
+                      ...prev.repository.releases.edges,
+                      ...fetchMoreResult.repository.releases.edges,
+                    ],
+                  },
+                },
+              };
+            },
+          });
+        };
+
+        return (
+          <Fragment>
+            {releases.map(({ node: release }) => (
+              <Release
+                key={release.tag.name}
+                version={release.tag.name}
+                notes={release.description}
+                date={release.publishedAt}
+              />
+            ))}
+            <Button onClick={loadMoreReleases}>Load More</Button>
+          </Fragment>
+        );
       }}
     </Query>
   </Fragment>
