9efaab246d9a7460cc3623af6831b79c6da6474e
Logan McAnsh <logan@mcan.sh>
2018-05-13 23:00:32 -0400

    add sourcemaps, next-optimized-images, next-compose-plugins
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const withPlugins = require('next-compose-plugins');
 const NextWorkboxWebpackPlugin = require('next-workbox-webpack-plugin');
+const optimizedImages = require('next-optimized-images');
+const sourceMaps = require('@zeit/next-source-maps');
 
-module.exports = {
+module.exports = withPlugins([[optimizedImages], [sourceMaps]], {
   webpack: (config, { isServer, dev, buildId, config: { distDir } }) => {
     if (!isServer && !dev) {
       config.plugins.push(
@@ -13,4 +16,4 @@ module.exports = {
     config.node = { fs: 'empty' };
     return config;
   },
-};
+});
