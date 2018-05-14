ff81803b75a35998bfa51978cb558b1ba369b126
Logan McAnsh <logan@mcan.sh>
2018-05-13 23:17:14 -0400

    use mime-types package to get mimetype, move package to dependencies
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- utils/webp.js
+++ utils/webp.js
@@ -1,7 +1,9 @@
+import { lookup } from 'mime-types';
+
 const webp = image => {
   const imageRegex = /png|jpg/;
   const url = image.replace(imageRegex, 'webp');
-  const [type] = image.match(imageRegex);
+  const type = lookup(image);
 
   return { url, type };
 };
