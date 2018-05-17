346c3527c2f13f5d9979ffa756b2f27201b224fd
Logan McAnsh <logan@mcan.sh>
2018-05-16 20:43:47 -0400

    when NODE_ENV === 'development' just render the page without caching
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- server/caching.js
+++ server/caching.js
@@ -8,32 +8,36 @@ const cache = new Cache({
 const getCacheKey = req => `${req.url}`;
 
 const renderAndCache = async ({ app, req, res, pagePath, queryParams }) => {
-  const key = getCacheKey(req);
+  if (process.env.NODE_ENV !== 'development') {
+    const key = getCacheKey(req);
 
-  // If we have a page in the cache, let's serve it
-  if (cache.has(key)) {
-    res.setHeader('x-cache', 'HIT');
-    res.end(cache.get(key));
-    return;
-  }
-
-  try {
-    // If not let's render the page into HTML
-    const html = await app.renderToHTML(req, res, pagePath, queryParams);
-
-    // Something is wrong with the request, let's skip the cache
-    if (res.statusCode !== 200) {
-      res.end(html);
+    // If we have a page in the cache, let's serve it
+    if (cache.has(key)) {
+      res.setHeader('x-cache', 'HIT');
+      res.end(cache.get(key));
       return;
     }
 
-    // Let's cache this page
-    cache.set(key, html);
+    try {
+      // If not let's render the page into HTML
+      const html = await app.renderToHTML(req, res, pagePath, queryParams);
 
-    res.setHeader('x-cache', 'MISS');
-    res.end(html);
-  } catch (err) {
-    app.renderError(err, req, res, pagePath, queryParams);
+      // Something is wrong with the request, let's skip the cache
+      if (res.statusCode !== 200) {
+        res.end(html);
+        return;
+      }
+
+      // Let's cache this page
+      cache.set(key, html);
+
+      res.setHeader('x-cache', 'MISS');
+      res.end(html);
+    } catch (err) {
+      app.renderError(err, req, res, pagePath, queryParams);
+    }
+  } else {
+    app.render(req, res, pagePath, queryParams);
   }
 };
 
