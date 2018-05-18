e5375d9311fd43155a8b363ad2d2363d2c776607
Logan McAnsh <logan@mcan.sh>
2018-05-18 12:59:23 -0400

    add bundle analyzer
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- next.config.js
+++ next.config.js
@@ -2,10 +2,38 @@ const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 const sourceMaps = require('@zeit/next-source-maps');
 const withOffline = require('next-offline');
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer'); // eslint-disable-line
 
-module.exports = withPlugins([[optimizedImages], [sourceMaps], [withOffline]], {
-  webpack: config => {
-    config.node = { fs: 'empty' };
-    return config;
-  },
-});
+module.exports = withPlugins(
+  [
+    [optimizedImages],
+    [sourceMaps],
+    [withOffline],
+    [
+      withBundleAnalyzer,
+      {
+        analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+        analyzeBrowser: ['browser', 'both'].includes(
+          process.env.BUNDLE_ANALYZE
+        ),
+        bundleAnalyzerConfig: {
+          server: {
+            analyzerMode: 'static',
+            reportFilename: '../../bundles/server.html',
+          },
+          browser: {
+            analyzerMode: 'static',
+            reportFilename: '../bundles/client.html',
+          },
+        },
+      },
+    ],
+  ],
+  {
+    webpack: config => {
+      config.node = { fs: 'empty' };
+      return config;
+    },
+  }
+);
