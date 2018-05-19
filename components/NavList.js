e620c75651b970e949cbd7fd9e9edb65c72dbafc
Logan McAnsh <logan@mcan.sh>
2018-05-19 10:31:53 -0400

    remove regular next/link import
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- components/NavList.js
+++ components/NavList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Link from 'next/link';
 import isAbsoluteUrl from 'is-absolute-url';
 import { FormattedMessage } from 'react-intl';
 import Link from './LinkWithData';
