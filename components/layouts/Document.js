28d25d9cc88fd40688a8df08f7aff7a9a80c985d
Logan McAnsh <logan@mcan.sh>
2018-05-19 12:51:25 -0400

    move global styles and ThemeProvider to _app.js
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- components/layouts/Document.js
+++ components/layouts/Document.js
@@ -2,9 +2,7 @@ import React, { Component, Fragment } from 'react';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import PropTypes from 'prop-types';
-import { ThemeProvider } from 'styled-components';
 import Navigation from '../Navigation';
-import colors from '../../theme';
 import Footer from '../Footer';
 import { version, repository } from '../../package.json';
 import { initGA, logPageView } from '../../lib/analytics';
@@ -37,108 +35,17 @@ class Document extends Component {
   render() {
     const { children } = this.props;
     return (
-      <ThemeProvider theme={colors}>
-        <Fragment>
-          <Navigation />
-          {children}
-          <Footer />
-          <style jsx global>{`
-            * {
-              box-sizing: border-box;
-              margin: 0;
-            }
-
-            html {
-              font-size: 10px;
-            }
-
-            body {
-              font-family: system-ui, -apple-system, BlinkMacSystemFont,
-                'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell',
-                'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif,
-                'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-              font-weight: 400;
-              margin: 0;
-              background: ${colors.background};
-            }
-
-            ::selection {
-              background: ${colors.primary};
-              color: white;
-            }
-
-            a {
-              color: ${colors.primary};
-              text-decoration-skip: ink;
-              transition: 300ms all ease-in-out;
-            }
-
-            a:hover {
-              color: ${colors.secondary};
-            }
-
-            a::selection {
-              color: white;
-            }
-
-            #nprogress {
-              pointer-events: none;
-            }
-
-            #nprogress .bar {
-              background: ${colors.primary};
-
-              position: fixed;
-              z-index: 1031;
-              top: 0;
-              left: 0;
-
-              width: 100%;
-              height: 0.2rem;
-            }
-
-            #nprogress {
-              pointer-events: none;
-            }
-
-            #nprogress .bar {
-              background: ${colors.primary};
-              position: fixed;
-              z-index: 1031;
-              top: 0;
-              left: 0;
-              width: 100%;
-              height: 0.2rem;
-            }
-
-            #nprogress .peg {
-              display: block;
-              position: absolute;
-              right: 0;
-              width: 10rem;
-              height: 100%;
-              box-shadow: 0 0 1rem ${colors.primary},
-                0 0 0.5rem ${colors.primary};
-              opacity: 1;
-              transform: rotate(3deg) translate(0, -0.4rem);
-            }
-
-            .nprogress-custom-parent {
-              overflow: hidden;
-              position: relative;
-            }
-
-            .nprogress-custom-parent #nprogress .spinner,
-            .nprogress-custom-parent #nprogress .bar {
-              position: absolute;
-            }
-          `}</style>
-        </Fragment>
-      </ThemeProvider>
+      <Fragment>
+        <Navigation />
+        {children}
+        <Footer />
+      </Fragment>
     );
   }
 }
 
-Document.propTypes = { children: PropTypes.node.isRequired };
+Document.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default withIntl(Document);
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,105 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import App, { Container } from 'next/app';
 import Raven from 'raven';
+import { ThemeProvider, injectGlobal } from 'styled-components';
+import colors from '../theme';
 import { version } from '../package.json';
 import Document from '../components/layouts/Document';
 import Meta from '../components/Meta';
 
 const isDev = process.env.NODE_ENV === 'development';
 
-export default class MyApp extends App {
+injectGlobal`
+  html {
+    font-size: 10px;
+    box-sizing: border-box;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+    margin: 0;
+  }
+
+
+  body {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+    font-weight: 400;
+    margin: 0;
+    background: ${colors.background};
+  }
+
+  ::selection {
+    background: ${colors.primary};
+    color: white;
+  }
+
+  a {
+    color: ${colors.primary};
+    text-decoration-skip: ink;
+    transition: 300ms all ease-in-out;
+  }
+
+  a:hover {
+    color: ${colors.secondary};
+  }
+
+  a::selection {
+    color: white;
+  }
+
+  #nprogress {
+    pointer-events: none;
+  }
+
+  #nprogress .bar {
+    background: ${colors.primary};
+    position: fixed;
+    z-index: 1031;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 0.2rem;
+  }
+
+  #nprogress {
+    pointer-events: none;
+  }
+
+  #nprogress .bar {
+    background: ${colors.primary};
+    position: fixed;
+    z-index: 1031;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 0.2rem;
+  }
+
+  #nprogress .peg {
+    display: block;
+    position: absolute;
+    right: 0;
+    width: 10rem;
+    height: 100%;
+    box-shadow: 0 0 1rem ${colors.primary}, 0 0 0.5rem ${colors.primary};
+    opacity: 1;
+    transform: rotate(3deg) translate(0, -0.4rem);
+  }
+
+  .nprogress-custom-parent {
+    overflow: hidden;
+    position: relative;
+  }
+
+  .nprogress-custom-parent #nprogress .spinner,
+  .nprogress-custom-parent #nprogress .bar {
+    position: absolute;
+  }
+`;
+
+class MyApp extends App {
   constructor(props) {
     super(props);
 
@@ -51,3 +143,20 @@ export default class MyApp extends App {
     );
   }
 }
+          <Fragment>
+            <Meta />
+            {statusCode ? (
+              <Error statusCode={statusCode} />
+            ) : (
+              <Document>
+                <Component {...pageProps} />
+              </Document>
+            )}
+          </Fragment>
+        </ThemeProvider>
+      </Container>
+    );
+  }
+}
+
+export default MyApp;
