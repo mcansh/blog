fedf4376014cbdcb0fb518484d6dac2621604cf2
Logan McAnsh <logan@mcan.sh>
2018-06-05 18:23:27 -0400

    convert posts wrapper to styled component
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import styled from 'styled-components';
 import PostCard from '../components/PostCard';
 import posts from '../posts.json';
 import Header from '../components/Header';
@@ -6,46 +7,40 @@ import Header from '../components/Header';
 const [latest] = posts;
 const { id: latestid } = latest;
 
+const PostsWrapper = styled.div`
+  margin: 0 auto;
+  width: 95%;
+  padding: 9rem 0 0 0;
+  padding-left: env(safe-area-inset-left);
+  padding-right: env(safe-area-inset-right);
+  padding-left: constant(safe-area-inset-left);
+  padding-right: constant(safe-area-inset-right);
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr));
+  grid-gap: 2rem;
+
+  @media (min-width: 768px) {
+    max-width: 750px;
+    width: auto;
+  }
+  @media (min-width: 992px) {
+    max-width: 970px;
+    width: auto;
+  }
+  @media (min-width: 1200px) {
+    max-width: 1170px;
+    width: auto;
+  }
+`;
+
 const Index = () => (
   <Fragment>
     <Header id={latestid} link={latestid} />
-    <div>
+    <PostsWrapper>
       {posts.map(({ image, date, title, id }) => (
         <PostCard key={id} image={image} date={date} title={title} id={id} />
       ))}
-    </div>
-    <style jsx>{`
-      div {
-        margin: 0 auto;
-        width: 95%;
-        padding: 9rem 0 0 0;
-        padding-left: env(safe-area-inset-left);
-        padding-right: env(safe-area-inset-right);
-        padding-left: constant(safe-area-inset-left);
-        padding-right: constant(safe-area-inset-right);
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr));
-        grid-gap: 1rem;
-      }
-      @media (min-width: 768px) {
-        div {
-          max-width: 750px;
-          width: auto;
-        }
-      }
-      @media (min-width: 992px) {
-        div {
-          max-width: 970px;
-          width: auto;
-        }
-      }
-      @media (min-width: 1200px) {
-        div {
-          max-width: 1170px;
-          width: auto;
-        }
-      }
-    `}</style>
+    </PostsWrapper>
   </Fragment>
 );
 
