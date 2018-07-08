41c32e28c638fea93b4cfa4731305688c46b90ff
Logan McAnsh <logan@mcan.sh>
2018-07-07 23:49:20 -0400

    convert Hamburger to styled components, update animation a bit
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- components/Hamburger.js
+++ components/Hamburger.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { defineMessages, injectIntl, intlShape } from 'react-intl';
+import styled from 'styled-components';
 import { NavigationContext } from './Navigation';
 
 const messages = defineMessages({
@@ -13,84 +14,84 @@ const messages = defineMessages({
   },
 });
 
+const MenuButton = styled.button`
+  position: ${props => (props.open ? 'fixed' : 'absolute')};
+  top: 2rem;
+  top: constant(safe-area-inset-top);
+  top: env(safe-area-inset-top);
+  left: 2rem;
+  left: constant(safe-area-inset-left);
+  left: env(safe-area-inset-left);
+  appearance: none;
+  height: 4rem;
+  width: 4rem;
+  background: none;
+  border: none;
+  padding: 0;
+  z-index: 4;
+  cursor: pointer;
+  overflow: hidden;
+
+  @supports (padding: max(0rem)) {
+    left: max(2rem, env(safe-area-inset-left));
+    top: max(2rem, env(safe-area-inset-top));
+  }
+`;
+
+const Lines = styled.span`
+  width: 100%;
+  height: 0.2rem;
+  background: ${props => (props.open ? 'transparent' : 'white')};
+  display: block;
+  position: relative;
+  border-radius: 0.5rem;
+  transition-property: background;
+  transition-timing-function: ${props => (props.open ? 'ease-in' : 'open')};
+  transition-duration: ${props => (props.open ? '350ms' : '150ms')};
+  left: 0;
+
+  &::before,
+  &::after {
+    content: '';
+    width: 100%;
+    height: 0.2rem;
+    position: absolute;
+    left: 0;
+    background: white;
+    border-radius: 0.5rem;
+    transition: 150ms all ease;
+    will-change: transform;
+  }
+
+  &::before {
+    top: ${props => (props.open ? 0 : '-1rem')};
+    transform: ${props => (props.open ? 'rotate(45deg)' : 'none')};
+    transition-property: all;
+    transition-timing-function: ${props => (props.open ? 'ease-in' : 'open')};
+    transition-duration: ${props => (props.open ? '350ms' : '150ms')};
+  }
+
+  &::after {
+    top: ${props => (props.open ? 0 : '1rem')};
+    transition-property: all;
+    transition-timing-function: ${props => (props.open ? 'ease-in' : 'open')};
+    transition-duration: ${props => (props.open ? '350ms' : '150ms')};
+    transform: ${props => (props.open ? 'rotate(-45deg)' : 'none')};
+  }
+`;
+
 const Hamburger = ({ intl: { formatMessage } }) => (
   <NavigationContext.Consumer>
     {({ toggleNav, state: { navOpen } }) => (
-      <button
+      <MenuButton
         aria-label={formatMessage(
           navOpen ? messages.navOpenLabel : messages.navClosedLabel
         )}
         onClick={toggleNav}
+        open={navOpen}
       >
-        <span />
-        <style jsx>{`
-          button {
-            position: ${navOpen ? 'fixed' : 'absolute'};
-            top: 2rem;
-            top: constant(safe-area-inset-top);
-            top: env(safe-area-inset-top);
-            left: 2rem;
-            left: constant(safe-area-inset-left);
-            left: env(safe-area-inset-left);
-            appearance: none;
-            height: 4rem;
-            width: 4rem;
-            background: none;
-            border: none;
-            padding: 0;
-            z-index: 4;
-            cursor: pointer;
-            overflow: hidden;
-          }
-
-          @supports (padding: max(0rem)) {
-            button {
-              left: max(2rem, env(safe-area-inset-left));
-              top: max(2rem, env(safe-area-inset-top));
-            }
-          }
-
-          button span {
-            width: 100%;
-            height: 0.2rem;
-            background: white;
-            display: block;
-            position: relative;
-            border-radius: 0.5rem;
-            transition: ${navOpen ? '350ms all ease-in' : '150ms all ease'};
-            will-change: transform;
-            top: ${navOpen ? 0 : '-1rem'};
-            transform: ${navOpen ? 'rotate(45deg)' : 'none'};
-          }
-
-          button span::before,
-          button span::after {
-            content: '';
-            width: 100%;
-            height: 0.2rem;
-            position: absolute;
-            left: 0;
-            background: white;
-            border-radius: 0.5rem;
-            transition: 150ms all ease;
-            will-change: transform;
-          }
-
-          button span::before {
-            top: ${navOpen ? 0 : '1rem'};
-            visibility: ${navOpen ? 'hidden' : 'visible'};
-            opacity: ${navOpen ? 0 : 1};
-            transform: ${navOpen ? 'rotate(-45deg)' : 'none'};
-            transition: ${navOpen ? '350ms all ease-in' : '150ms all ease'};
-          }
-
-          button span::after {
-            top: ${navOpen ? 0 : '2rem'};
-            transition: ${navOpen ? '350ms all ease-in' : '150ms all ease'};
-            transform: ${navOpen ? 'rotate(-90deg)' : 'none'};
-          }
-        `}</style>
-      </button>
+        <Lines open={navOpen} />
+      </MenuButton>
     )}
   </NavigationContext.Consumer>
 );
