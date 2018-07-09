ba1881a01113ba7ddc01483b44440e20bb75cb24
Logan McAnsh <logan@mcan.sh>
2018-07-08 22:58:33 -0400

    update ssr caching
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- server/caching.js
+++ server/caching.js
@@ -1,43 +1,38 @@
-const Cache = require('tmp-cache');
+import Cache from 'tmp-cache';
+import Raven from 'raven';
 
 const cache = new Cache({
   max: 100,
   maxAge: 1000 * 60 * 60, // 1 hour
 });
 
-const getCacheKey = req => `${req.url}`;
-
 const renderAndCache = async ({ app, req, res, pagePath, queryParams }) => {
-  if (process.env.NODE_ENV !== 'development') {
-    const key = getCacheKey(req);
+  const key = req.url;
 
-    // If we have a page in the cache, let's serve it
-    if (cache.has(key)) {
-      res.setHeader('x-cache', 'HIT');
-      res.end(cache.get(key));
-      return;
+  // If we have a page in the cache, let's serve it
+  if (cache.has(key)) {
+    res.setHeader('x-cache', 'HIT');
+    const page = cache.get(key);
+    return res.end(page);
+  }
+
+  try {
+    // If not let's render the page into HTML
+    const html = await app.renderToHTML(req, res, pagePath, queryParams);
+
+    // If something is wrong with the request, let's skip the cache
+    if (res.statusCode !== 200) {
+      return res.end(html);
     }
 
-    try {
-      // If not let's render the page into HTML
-      const html = await app.renderToHTML(req, res, pagePath, queryParams);
+    // Let's cache this page
+    cache.set(key, html);
 
-      // Something is wrong with the request, let's skip the cache
-      if (res.statusCode !== 200) {
-        res.end(html);
-        return;
-      }
-
-      // Let's cache this page
-      cache.set(key, html);
-
-      res.setHeader('x-cache', 'MISS');
-      res.end(html);
-    } catch (err) {
-      app.renderError(err, req, res, pagePath, queryParams);
-    }
-  } else {
-    app.render(req, res, pagePath, queryParams);
+    res.setHeader('x-cache', 'MISS');
+    return res.end(html);
+  } catch (error) {
+    Raven.captureException(error);
+    return app.renderError(error, req, res, pagePath, queryParams);
   }
 };
 
