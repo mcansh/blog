0f315c986ce6d89eaea5140969bbdc09daae8e59
Logan McAnsh <logan@mcan.sh>
2018-07-19 22:03:29 -0400

    update atom feed formatting
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- server/atom.js
+++ server/atom.js
@@ -5,32 +5,32 @@ import { name, email } from '../utils/authorInfo';
 
 const [newestPost] = posts;
 
-const atom = () =>
-  `<?xml version="1.0" encoding="utf-8"?>
-    <feed xmlns="http://www.w3.org/2005/Atom">
-      <title>${name}</title>
-      <subtitle>${description}</subtitle>
-      <link href="${homepage}/atom" rel="self"/>
-      <link href="${homepage}"/>
-      <updated>${YYYYMMDD(newestPost.date)}</updated>
-      <id>${homepage}</id>
-      <author>
-        <name>${name}</name>
-        <email>${email}</email>
-      </author>
-      ${posts
-        .map(
-          ({ date, title, id }) => `
-            <entry>
-              <id>${id}</id>
-              <title>${title}</title>
-              <link href="${homepage}/${id}"/>
-              <updated>${YYYYMMDD(date)}</updated>
-            </entry>
-          `
-        )
-        .join('')}
-    </feed>
-  `;
+const atom = () => `
+  <?xml version="1.0" encoding="utf-8"?>
+  <feed xmlns="http://www.w3.org/2005/Atom">
+    <title>${name}</title>
+    <subtitle>${description}</subtitle>
+    <link href="${homepage}/atom" rel="self"/>
+    <link href="${homepage}"/>
+    <updated>${YYYYMMDD(newestPost.date)}</updated>
+    <id>${homepage}</id>
+    <author>
+      <name>${name}</name>
+      <email>${email}</email>
+    </author>
+    ${posts
+      .map(
+        ({ date, title, id }) => `
+      <entry>
+        <id>${id}</id>
+        <title>${title}</title>
+        <link href="${homepage}/${id}"/>
+        <updated>${YYYYMMDD(date)}</updated>
+      </entry>
+    `
+      )
+      .join('')}
+  </feed>
+`;
 
 export default atom;
