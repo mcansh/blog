0b807485cfbbbb5c3a389c0be7ca9e899ce6eb76
Logan McAnsh <logan@mcan.sh>
2018-07-28 10:35:08 -0400

    rename sample.env to sample.env-config.js
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ sample.env-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'process.env.NODE_ENV': 'production',
+  'process.env.TWITTER': 'twitter',
+  'process.env.GITHUB': 'github',
+  'process.env.INSTAGRAM': 'instagram',
+  'process.env.EMAIL': 'email',
+  'process.env.PORT': '3000',
+  'process.env.SENTRY': 'https://sentry.io',
+  'process.env.ANALYTICS': 'analytics.google.com',
+  'process.env.TOKEN': 'https://github.com/settings/tokens/new  ',
+};
