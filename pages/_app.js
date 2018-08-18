6040e94d005441587419e80ee3fd5a5809be2988
Logan McAnsh <logan@mcan.sh>
2018-08-18 13:00:04 -0400

    remove logging of messages
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/_app.js
+++ pages/_app.js
@@ -144,7 +144,6 @@ class MyApp extends App {
     // eslint-disable-next-line no-underscore-dangle
     const { locale, messages } = req || window.__NEXT_DATA__.props;
     const now = Date.now();
-    console.log(messages);
 
     return { pageProps, locale, messages, now };
   }
