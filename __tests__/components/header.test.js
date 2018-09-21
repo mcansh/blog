8a1546ee1a7610bb495176f36802dc5335ef13c4
Logan McAnsh <logan@mcan.sh>
2018-09-20 21:38:42 -0400

    remove snapshot tests
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- __tests__/components/header.test.js
+++ __tests__/components/header.test.js
@@ -1,7 +1,6 @@
 /* eslint-env jest */
 import { shallow } from 'enzyme';
 import React from 'react';
-import renderer from 'react-test-renderer';
 import 'jest-styled-components';
 
 import Header, { Title } from '../../components/Header';
@@ -23,21 +22,5 @@ describe('Header Component', () => {
         .render()
         .text()
     ).toBe('BLOG');
-  });
-});
-
-describe('Header component with Snapshot Testing', () => {
-  it('shows custom text and image', () => {
-    const component = renderer.create(
-      <Header
-        title="BLOG"
-        image={{
-          imageUrl:
-            'http://localhost:3000/static/images/posts/1*Wmv8hfi_bTHuHyV5CawnCw.jpg',
-        }}
-      />
-    );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
   });
 });
