53f220a2e1bca774647977528c54f4d217e93636
Logan McAnsh <logan@mcan.sh>
2018-10-13 20:22:34 -0400

    convert Code components to styled-components
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- components/Code.js
+++ components/Code.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import OceanicNext from 'prism-react-renderer/themes/oceanicNext';
@@ -39,57 +40,21 @@ export const Code = ({ language, children }) => (
   </Highlight>
 );
 
-// const Code = ({ language, syntax, children }) => {
-//   if (!language || !syntax) {
-//     throw new Error('Please define a `language` and `syntax`.');
-//   }
+export const InlineCode = styled.code`
+  font-size: 0.85em;
+  padding: 0.125rem 0.25rem;
+  background: rgba(85, 85, 86, 0.05);
+  color: #df0050;
+  box-shadow: 0 0 0 0.1rem rgba(85, 85, 86, 0.2);
+  border-radius: 0.2rem;
+  font-family: native, menlo, monospace;
+  hyphens: none;
+`;
 
-//   registerLanguage(language, syntax);
-
-//   const styles = {
-//     margin: '3rem 0',
-//     padding: '1.4rem',
-//     borderRadius: '0.4rem',
-//     width: '100%',
-//     boxSizing: 'border-box',
-//     wordWrap: 'normal',
-//     fontSize: '1.6rem',
-//     lineHeight: '1.5',
-//     WebkitOverflowScrolling: 'touch',
-//     background: 'white',
-//     fontFamily: 'native, menlo, monospace',
-//   };
-
-//   return (
-//     <SyntaxHighlighter language={language} style={github} customStyle={styles}>
-//       {children.trim()}
-//     </SyntaxHighlighter>
-//   );
-// };
-
-export const InlineCode = ({ children }) => (
-  <code>
-    {children}
-    <style jsx>{`
-      code {
-        font-size: 0.85em;
-        padding: 0.125rem 0.25rem;
-        background: rgba(85, 85, 86, 0.05);
-        color: #df0050;
-        box-shadow: 0 0 0 0.1rem rgba(85, 85, 86, 0.2);
-        border-radius: 0.2rem;
-        font-family: native, menlo, monospace;
-        hyphens: none;
-      }
-    `}</style>
-  </code>
-);
-
-// Code.propTypes = {
-//   language: PropTypes.string.isRequired,
-//   syntax: PropTypes.func.isRequired,
-//   children: PropTypes.node.isRequired,
-// };
+Code.propTypes = {
+  language: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
 InlineCode.propTypes = {
   children: PropTypes.node.isRequired,
