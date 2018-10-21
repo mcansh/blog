f01021d2892f279d36f04f98a23db6e147ff077b
Logan McAnsh <logan@mcan.sh>
2018-10-21 03:49:27 -0400

    add isomorphic version of path for client renders
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- utils/webp.js
+++ utils/webp.js
@@ -1,10 +1,10 @@
 // @flow
 
 import mime from 'mime/lite';
-import { parse } from 'path';
+import path from 'path';
 
 const webp = (image: string): { url: string, type: string } => {
-  const { name } = parse(image);
+  const { name } = path.parse(image);
 
   const type = mime.getType(image);
 
