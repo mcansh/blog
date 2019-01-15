014d2e36a8bbf188f08340def42bcade3343ac9f
Logan McAnsh <logan@mcan.sh>
2019-01-15 01:35:52 -0500

    indent
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- components/Styles/NProgress.tsx
+++ components/Styles/NProgress.tsx
@@ -48,84 +48,85 @@ interface Props {
 }
 
 const Progress = createGlobalStyle<Props>`
-#nprogress {
-  pointer-events: none;
-}
-
-#nprogress .bar {
-  background: ${props => props.color};
-
-  position: fixed;
-  z-index: 1031;
-  top: 0;
-  left: 0;
-
-  width: 100%;
-  height: 2px;
-}
-
-#nprogress .peg {
-  display: block;
-  position: absolute;
-  right: 0px;
-  width: 100px;
-  height: 100%;
-  box-shadow: 0 0 10px ${props => props.color}, 0 0 5px ${props => props.color};
-  opacity: 1;
-
-  --webkit-transform: rotate(3deg) translate(0px, -4px);
-  --ms-transform: rotate(3deg) translate(0px, -4px);
-  transform: rotate(3deg) translate(0px, -4px);
-}
-
-#nprogress .spinner {
-  display: ${props => (props.spinner ? 'block' : 'none')};
-  position: fixed;
-  z-index: 1031;
-  top: 15px;
-  right: 15px;
-}
-
-#nprogress .spinner-icon {
-  width: 18px;
-  height: 18px;
-  box-sizing: border-box;
-
-  border: solid 2px transparent;
-  border-top-color: ${props => props.color};
-  border-left-color: ${props => props.color};
-  border-radius: 50%;
-
-  --webkit-animation: nprogresss-spinner 400ms linear infinite;
-  animation: nprogress-spinner 400ms linear infinite;
-}
-
-.nprogress-custom-parent {
-  overflow: hidden;
-  position: relative;
-}
-
-.nprogress-custom-parent #nprogress .spinner,
-.nprogress-custom-parent #nprogress .bar {
-  position: absolute;
-}
-
-@-webkit-keyframes nprogress-spinner {
-  0% {
-    --webkit-transform: rotate(0deg);
+  #nprogress {
+    pointer-events: none;
   }
-  100% {
-    --webkit-transform: rotate(360deg);
+
+  #nprogress .bar {
+    background: ${props => props.color};
+
+    position: fixed;
+    z-index: 1031;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 2px;
   }
-}
-@keyframes nprogress-spinner {
-  0% {
-    transform: rotate(0deg);
+
+  #nprogress .peg {
+    display: block;
+    position: absolute;
+    right: 0px;
+    width: 100px;
+    height: 100%;
+    box-shadow: 0 0 10px ${props => props.color}, 0 0 5px ${props =>
+  props.color};
+    opacity: 1;
+
+    --webkit-transform: rotate(3deg) translate(0px, -4px);
+    --ms-transform: rotate(3deg) translate(0px, -4px);
+    transform: rotate(3deg) translate(0px, -4px);
   }
-  100% {
-    transform: rotate(360deg);
+
+  #nprogress .spinner {
+    display: ${props => (props.spinner ? 'block' : 'none')};
+    position: fixed;
+    z-index: 1031;
+    top: 15px;
+    right: 15px;
   }
-}
+
+  #nprogress .spinner-icon {
+    width: 18px;
+    height: 18px;
+    box-sizing: border-box;
+
+    border: solid 2px transparent;
+    border-top-color: ${props => props.color};
+    border-left-color: ${props => props.color};
+    border-radius: 50%;
+
+    --webkit-animation: nprogresss-spinner 400ms linear infinite;
+    animation: nprogress-spinner 400ms linear infinite;
+  }
+
+  .nprogress-custom-parent {
+    overflow: hidden;
+    position: relative;
+  }
+
+  .nprogress-custom-parent #nprogress .spinner,
+  .nprogress-custom-parent #nprogress .bar {
+    position: absolute;
+  }
+
+  @-webkit-keyframes nprogress-spinner {
+    0% {
+      --webkit-transform: rotate(0deg);
+    }
+    100% {
+      --webkit-transform: rotate(360deg);
+    }
+  }
+  @keyframes nprogress-spinner {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;
 
 const NProgressContainer = ({
