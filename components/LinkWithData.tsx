36fc13c10c97fe36fcc630cabff0fd45dc392bca
Logan McAnsh <logan@mcan.sh>
2019-01-21 21:30:37 -0500

    linted :muscle:
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- components/LinkWithData.tsx
+++ components/LinkWithData.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Link, { LinkProps } from 'next/link';
 import Router from 'next/router';
 import { format, resolve, parse, UrlObject } from 'url';
