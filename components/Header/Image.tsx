6de82c64470719c95f52afc9b041bdbd3bcc0187
Logan McAnsh <logan@mcan.sh>
2019-02-02 13:35:57 -0500

    rename unsplashParams file
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- components/Header/Image.tsx
+++ components/Header/Image.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import unsplashParams from '../../utils';
+import unsplashParams from '../../utils/unsplashParams';
 import { ImageTypes } from './index';
 import getCloudinaryURL from '../../utils/getCloudinaryURL';
 
--- components/PostCard/index.tsx
+++ components/PostCard/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
-import unsplashParams from '../../utils';
+import unsplashParams from '../../utils/unsplashParams';
 import {
   Post,
   ImageWrap,
