fce3fd7d94245b29e3fc0aacc1ce0cab3470d2b7
Logan McAnsh <logan@mcan.sh>
2019-02-02 13:36:38 -0500

    update babelrc
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { version } = require('./package.json');
+
+const env = {
+  'process.env.VERSION': version,
+  'process.env.NODE_ENV': process.env.NODE_ENV,
+  'process.env.ANALYTICS': process.env.ANALYTICS,
+  'process.env.GITHUB_TOKEN': process.env.GITHUB_TOKEN,
+};
+
 module.exports = {
   presets: ['next/babel', '@zeit/next-typescript/babel'],
-  plugins: ['styled-components', 'polished', 'inline-dotenv', 'react-intl'],
+  plugins: [
+    'styled-components',
+    'polished',
+    'inline-dotenv',
+    'react-intl',
+    ['transform-define', env],
+  ],
 };
