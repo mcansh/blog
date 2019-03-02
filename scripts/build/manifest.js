e22bb8777c9b06972f90e518633b9fe74b3d10a5
Logan McAnsh <logan@mcan.sh>
2019-03-01 20:21:06 -0500

    Static file builder (#498)
    * generate sitemap again
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * build tweak
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * build static files during next build
    
    holding off on merging until i can get it to work with ts-node
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ scripts/build/manifest.js
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs');
+const path = require('path');
+const {
+  description,
+  productName,
+  productShortName,
+} = require('../../package.json');
+
+const OUT_DIR = path.join(__dirname, '..', '..', 'static');
+
+const iconSizes = [72, 96, 128, 144, 256, 512];
+
+const icons = iconSizes.map(icon => ({
+  src: `/static/images/logo/logo-${icon}.png`,
+  sizes: `${icon}x${icon}`,
+}));
+
+const json = {
+  name: productName,
+  short_name: productShortName,
+  description,
+  start_url: '/?homescreen=1',
+  background_color: '#6c16c7',
+  theme_color: '#6c16c7',
+  display: 'standalone',
+  icons: JSON.stringify(icons),
+};
+
+module.exports = () => {
+  fs.writeFileSync(
+    path.join(OUT_DIR, 'manifest.json'),
+    JSON.stringify(json, null, 2)
+  );
+};
