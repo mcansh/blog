0bed4b00b87fc5dd445558a870deac5cab66c23a
Logan McAnsh <logan@mcan.sh>
2019-05-15 21:31:38 -0400

    i can remove this thanks to jest using ts by default now
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- test/jest.testing.js
+++ test/jest.testing.js
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const { join } = require('path');
 
 const rootDir = join(__dirname, '..');
@@ -6,17 +5,12 @@ const rootDir = join(__dirname, '..');
 module.exports = {
   rootDir,
   displayName: 'test',
-  transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
-  },
   setupFilesAfterEnv: [
     'jest-dom/extend-expect',
     'react-testing-library/cleanup-after-each',
     'jest-styled-components',
     `${rootDir}/jest.setup.js`,
   ],
-  testMatch: ['**/*.test.+(js|jsx|ts|tsx)'],
-  moduleFileExtensions: ['js', 'json', 'jsx', 'node', 'ts', 'tsx'],
   testPathIgnorePatterns: [
     `${rootDir}/.next/`,
     `${rootDir}/node_modules/`,
