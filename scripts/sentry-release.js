75489cdd6a947da451bf65a38eb08edac984b7cc
Logan McAnsh <logan@mcan.sh>
2019-05-27 18:43:43 -0400

    update sentry release
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- scripts/sentry-release.js
+++ scripts/sentry-release.js
@@ -1,30 +1,24 @@
 const { execSync } = require('child_process');
 const args = require('args');
-const { version } = require('../package.json');
+const { version: PKGVersion } = require('../package.json');
 
 async function sentryRelease() {
   args
     .option(
-      'project_version',
+      'version',
       "The version you'd like to register on Sentry",
-      version
+      PKGVersion
     )
     .option(
-      'last_release_sha',
-      'The commit sha of the last commit from the previous release'
-    )
-    .option(
-      'this_release_sha',
-      'The commit sha of the last commit from the current release'
+      'sha',
+      'last commit from release previous sha..last commit from the new release'
     );
 
-  const flags = args.parse(process.argv, {
+  const { sha, version } = args.parse(process.argv, {
     name: 'Sentry Releaser',
   });
 
   const { SENTRY_ORG, SENTRY_AUTH_TOKEN } = process.env;
-
-  console.log(flags);
 
   if (!SENTRY_ORG) {
     throw new Error('A `SENTRY_ORG` environment variable is required');
@@ -35,18 +29,18 @@ async function sentryRelease() {
   }
 
   try {
-    execSync(`sentry-cli releases new -p blog ${flags.p}`);
-    console.log(`Release created for ${flags.p}!`);
+    execSync(`sentry-cli releases new -p blog ${version}`);
+    console.log(`Release created for ${version}!`);
   } catch (error) {
     throw new Error(`An error occured creating that release :(`, error);
   }
 
-  if (!flags.lastReleaseSha || !flags.thisReleaseSha) {
+  if (!sha) {
     console.log(
       `no commit shas were provided, letting sentry figure that out...`
     );
     try {
-      execSync(`sentry-cli releases set-commits ${flags.p} --auto`);
+      execSync(`sentry-cli releases set-commits ${version} --auto`);
     } catch (error) {
       throw new Error(
         `An error occured setting the commits for that release :(`,
@@ -54,11 +48,10 @@ async function sentryRelease() {
       );
     }
   } else {
+    const [last, current] = sha.split('..');
     try {
       execSync(
-        `sentry-cli releases set-commits ${flags.p} --commit "mcansh/blog@${
-          flags.lastReleaseSha
-        }..${flags.thisReleaseSha}"`
+        `sentry-cli releases set-commits ${version} --commit "mcansh/blog@${last}..${current}"`
       );
     } catch (error) {
       throw new Error(
@@ -68,7 +61,7 @@ async function sentryRelease() {
     }
   }
 
-  execSync(`sentry-cli releases deploys ${flags.p} new -e production`);
+  execSync(`sentry-cli releases deploys ${version} new -e production`);
 }
 
 sentryRelease();
