a1ea8daf3ae2015a13a4199f1b48b8cc004a5bf2
Logan McAnsh <logan@mcan.sh>
2019-10-27 12:44:35 -0400

    chore(deps): bump to latest
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,37 +1,6 @@
-const {
-  processors,
-  ...config
-} = require('@mcansh/eslint-config/stylelint.config');
-
 module.exports = {
-  processors: [
-    [
-      'stylelint-processor-styled-components',
-      {
-        moduleName: 'styled-components',
-        importName: 'default',
-        strict: false,
-        ignoreFiles: [],
-        parserPlugins: [
-          'jsx',
-          'objectRestSpread',
-          ['decorators', { decoratorsBeforeExport: true }],
-          'classProperties',
-          'exportExtensions',
-          'asyncGenerators',
-          'functionBind',
-          'functionSent',
-          'dynamicImport',
-          'optionalCatchBinding',
-          'optionalChaining',
-          'nullishCoalescingOperator',
-        ],
-      },
-    ],
-  ],
-  ...config,
+  ...require('@mcansh/eslint-config/stylelint.config'),
   rules: {
-    ...config.rules,
     'selector-type-no-unknown': [true, { ignoreTypes: ['/^amp-/'] }],
     'selector-max-type': [0, { ignoreTypes: '/^((?!^i-amp-).)*$/' }],
     'selector-class-pattern': '^((?!^-amp-).)*$',
