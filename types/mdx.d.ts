2c3fe5d41b6028ce2a5cb4bb597ad8952129bd2d
Logan McAnsh <logan@mcan.sh>
2019-10-29 18:38:32 -0400

    chore(deps): bump to latest
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- types/mdx.d.ts
+++ types/mdx.d.ts
@@ -1,7 +1,6 @@
 declare module '*.mdx' {
   import { Post } from '~/components/post-card';
   const MDXComponent: (props) => JSX.Element;
-  const meta: Post;
-  export { meta };
+  export const meta: Post;
   export default MDXComponent;
 }
