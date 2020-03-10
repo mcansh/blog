6421fee34d74f74ee6e1f99d08c169948f5d0567
Logan McAnsh <logan@mcan.sh>
2020-03-10 19:18:26 -0400

    chore: back to fathom-client
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,27 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { NProgress } from '@mcansh/next-nprogress';
+import * as Fathom from 'fathom-client';
+import Router from 'next/router';
 
 import GlobalStyle from '~/components/styles/global-style';
 import { colors } from '~/config';
 import Document from '~/components/layouts/document';
 
+Router.events.on('routeChangeComplete', () => {
+  Fathom.trackPageview();
+});
+
 const App = ({ Component, pageProps }: AppProps) => {
   const statusCode = pageProps?.statusCode ?? 200;
+
+  React.useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      Fathom.load();
+      Fathom.setSiteId(process.env.FATHOM_SITE_ID);
+      Fathom.trackPageview();
+    }
+  }, []);
 
   return (
     <React.StrictMode>
--- types/window.d.ts
+++
@@ -1,6 +0,0 @@
-import { NEXT_DATA } from 'next/dist/next-server/lib/utils';
-
-interface Window {
-  GA_INITIALIZED: boolean;
-  __NEXT_DATA__: NEXT_DATA;
-}
