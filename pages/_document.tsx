0794e05dac65939d2105a845f31bb3315f137299
Logan McAnsh <logan@mcan.sh>
2020-03-10 18:13:13 -0400

    chore: use fathom code snippet for non js users
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -40,6 +40,28 @@ class MyDocument extends Document {
           {!inAmpMode && (
             <meta httpEquiv="Content-Security-Policy" content={csp} />
           )}
+
+          {process.env.NODE_ENV === 'production' && (
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                <!-- Fathom - simple website analytics - https://usefathom.com -->
+                (function(f, a, t, h, o, m){
+                  a[h]=a[h]||function(){
+                    (a[h].q=a[h].q||[]).push(arguments)
+                  };
+                  o=f.createElement('script'),
+                  m=f.getElementsByTagName('script')[0];
+                  o.async=1; o.src=t; o.id='fathom-script';
+                  m.parentNode.insertBefore(o,m)
+                })(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
+                fathom('set', 'siteId', '${process.env.FATHOM_SITE_ID}');
+                fathom('trackPageview');
+                <!-- / Fathom -->
+              `,
+              }}
+            />
+          )}
         </Head>
         <body>
           <Main />
