569a26b43c34060e3619e0213b79d6374f6bc41a
Logan McAnsh <logan@mcan.sh>
2020-03-22 12:25:27 -0400

    chore: run prettier over everything
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- utils/unsplash-params.ts
+++ utils/unsplash-params.ts
@@ -1,4 +1,4 @@
-const unsplashParams = (url?: string | null): string => {
+const unsplashParams = (url?: string | undefined): string => {
   if (url == null) return '';
   return url.startsWith('https://unsplash.com')
     ? `${url}?utm_source=unsplash&utm_medium=referral&utm_content=mcansh_blog`
