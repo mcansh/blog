7e82683507ab8ef788c70de61cb398b32e04a272
Logan McAnsh <logan@mcan.sh>
2020-03-26 22:50:31 -0400

    chore: update test coverage
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   testURL: 'http://localhost:3000/',
   coverageDirectory: './coverage/',
   collectCoverage: true,
+  collectCoverageFrom: [
+    'components/**/*.{js,jsx,ts,tsx}',
+    'pages/**/*.{js,jsx,ts,tsx}',
+    'utils/**/*.{js,jsx,ts,tsx}',
+  ],
   setupFilesAfterEnv: [
     '@testing-library/jest-dom/extend-expect',
     'jest-styled-components',
