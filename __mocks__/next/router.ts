20a031e0767b417a7b381e6be4686875fcc4e049
Logan McAnsh <logan@mcan.sh>
2020-03-28 17:34:41 -0400

    chore: update mocked next router
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ __mocks__/next/router.ts
@@ -0,0 +1,36 @@
+import { NextRouter } from 'next/router';
+
+function noop(..._args: any[]) {
+  return undefined;
+}
+
+const router: NextRouter = {
+  asPath: '/',
+  back: noop,
+  beforePopState: noop,
+  events: {
+    emit: noop,
+    off: noop,
+    on: noop,
+  },
+  isFallback: false,
+  pathname: '/',
+  prefetch: () => new Promise(resolve => resolve(undefined)),
+  push: () => new Promise(resolve => resolve(true)),
+  query: {},
+  reload: noop,
+  replace: () => new Promise(resolve => resolve(true)),
+  route: '/',
+};
+
+export function useRouter() {
+  return router;
+}
+
+export function withRouter(Component: React.FC) {
+  Component.defaultProps = {
+    ...Component.defaultProps,
+    router,
+  };
+  return Component;
+}
