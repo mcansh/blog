0794d558feaab9019ae50de6fe3104173298907d
Logan McAnsh <logan@mcan.sh>
2020-03-28 17:47:29 -0400

    chore: update styled components theme interface
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- @types/styled.d.ts
+++ @types/styled.d.ts
@@ -1,19 +1,17 @@
-/* eslint-disable no-duplicate-imports, import/no-duplicates */
 // Strongly type the styled-components theme
-import {} from 'styled-components';
 import { CSSProp } from 'styled-components';
 
 import theme from '../config';
-
-type ThemeInterface = typeof theme;
-
-declare module 'styled-components' {
-  type DefaultTheme = ThemeInterface;
-}
 
 // Enable css prop support globally
 declare module 'react' {
   interface Attributes {
     css?: CSSProp;
   }
+}
+
+declare module 'styled-components' {
+  type Theme = typeof theme;
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends Theme {}
 }
