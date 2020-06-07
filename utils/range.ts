09a1323e76b132c51a86758eedb438aa7067c5f9
Logan McAnsh <logan@mcan.sh>
2020-06-07 16:51:04 -0400

    test: add tests for range function

--- utils/range.ts
+++ utils/range.ts
@@ -1,4 +1,8 @@
 function range(start: number, end: number) {
+  if (start > end) {
+    throw new RangeError('end cannot be less than start');
+  }
+
   return Array(end - start + 1)
     .fill(null)
     .map((_, idx) => start + idx);
