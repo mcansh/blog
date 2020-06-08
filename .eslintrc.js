a8f888aea85275b374d960c828008691ba96267f
Logan McAnsh <logan@mcan.sh>
2020-06-08 11:16:36 -0400

    chore: cleanup eslint settings that config takes care of
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,10 @@
 module.exports = {
   extends: ['@mcansh/eslint-config/typescript'],
-  settings: {
-    'import/resolver': {
-      typescript: {},
-    },
-  },
   rules: {
     'react/jsx-filename-extension': [
       'warn',
       { extensions: ['.js', '.jsx', '.tsx', '.mdx'] },
     ],
-    'react/prop-types': 'off',
-    'no-underscore-dangle': ['error', { allow: ['__NEXT_DATA__'] }],
-    'no-console': ['error', { allow: ['warn', 'error'] }],
-    '@typescript-eslint/explicit-member-accessibility': 'error',
     'import/no-extraneous-dependencies': [
       'error',
       {
