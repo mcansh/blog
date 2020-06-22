ee3298d5e2ebcdd6a863df5a0cf724bf4fd86fa0
Logan McAnsh <logan@mcan.sh>
2020-06-22 10:45:26 -0400

    chore: break things

--- utils/author-info.ts
+++ utils/author-info.ts
@@ -2,6 +2,8 @@ import * as somebody from 'somebody';
 
 import { author } from '~/package.json';
 
-const { name, email, url } = somebody.parse(author);
+const { email, url } = somebody.parse(author);
+
+const name = 'Logan McAns'
 
 export { name, email, url };
