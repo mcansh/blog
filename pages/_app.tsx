ee1a57c0bb51c52a334ea0c98420bf3cc547467e
Logan McAnsh <logan@mcan.sh>
2020-07-08 11:43:46 -0400

    chore: use fathom-client
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+import * as Fathom from 'fathom-client';
+import Router from 'next/router';
 
 import GlobalStyle from '~/components/styles/global-style';
 import { colors } from '~/config';
 import Document from '~/components/layouts/document';
 import { NProgress } from '~/components/nprogress';
 
+Router.events.on('routeChangeComplete', () => {
+  Fathom.trackPageview();
+});
+
 const App = ({ Component, pageProps }: AppProps) => {
   const statusCode = pageProps?.statusCode ?? 200;
 
   React.useEffect(() => {
-    const tracker = window.document.createElement('script');
-    const firstScript = window.document.getElementsByTagName('script')[0];
-    tracker.defer = true;
-    tracker.setAttribute('site', process.env.FATHOM_SITE_ID);
-    tracker.setAttribute('spa', 'auto');
-    tracker.src = `${process.env.FATHOM_SUBDOMAIN}/script.js`;
-    firstScript.parentNode?.insertBefore(tracker, firstScript);
+    Fathom.load(process.env.FATHOM_SITE_ID, {
+      excludedDomains: ['localhost'],
+      url: `${process.env.FATHOM_SUBDOMAIN}/script.js`,
+    });
   }, []);
 
   return (
