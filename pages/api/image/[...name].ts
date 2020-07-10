c63c4f432fe446332c30260d0dd6c57572e93db5
Logan McAnsh <logan@mcan.sh>
2020-07-10 12:57:07 -0400

    feat(api/image): get mimetype from buffer
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/api/image/[...name].ts
+++ pages/api/image/[...name].ts
@@ -3,10 +3,16 @@ import path from 'path';
 
 import { NextApiHandler } from 'next';
 import sharp from 'sharp';
+import { fromBuffer } from 'file-type';
 
 import { getParams } from '~/utils/params';
 
 const denylist = ['heic', 'heif'];
+
+async function getFileTypeWithFallback(fileBuffer: Buffer, fallback: string) {
+  const result = await fromBuffer(fileBuffer);
+  return result ?? { ext: fallback, mime: `image/${fallback}` };
+}
 
 const handler: NextApiHandler = async (req, res) => {
   const supportsWebP = req.headers.accept?.includes('image/webp');
@@ -18,21 +24,22 @@ const handler: NextApiHandler = async (req, res) => {
   } = getParams(req.query);
 
   const filename = (req.query.name as string[]).join('/');
-  const format = explicitlyRequestedFormat
-    ? explicitlyRequestedFormat
-    : supportsWebP
-    ? 'webp'
-    : (filename as string).split('.')[1];
-
-  if (denylist.includes(format)) {
-    return res
-      .status(400)
-      .end(`requested format ${format} is not supported at this time`);
-  }
-
   const image = await fs.readFile(path.join(process.cwd(), 'public', filename));
 
-  const imageBuffer = Buffer.from(image);
+  const format = explicitlyRequestedFormat
+    ? {
+        mime: `image/${explicitlyRequestedFormat}`,
+        ext: explicitlyRequestedFormat,
+      }
+    : supportsWebP
+    ? { ext: 'webp', mime: `image/webp` }
+    : await getFileTypeWithFallback(image, (filename as string).split('.')[1]);
+
+  if (denylist.includes(format.ext)) {
+    return res
+      .status(400)
+      .end(`requested format ${format.ext} is not supported at this time`);
+  }
 
   const defaultOptions = {
     quality: quality ? Number(quality) : undefined,
@@ -42,15 +49,15 @@ const handler: NextApiHandler = async (req, res) => {
     width: width ? Number(width) : undefined,
   };
 
-  const sharped = sharp(imageBuffer)
-    .toFormat(format, defaultOptions)
+  const sharped = sharp(image)
+    .toFormat(format.ext, defaultOptions)
     .resize(defaultResizeOptions);
 
   const result = await sharped.toBuffer();
 
-  res.setHeader('Content-Type', `image/${format}`);
+  res.setHeader('Content-Type', format.mime);
   res.setHeader('Content-Length', result.byteLength);
-  return res.status(200).send(result);
+  return res.status(200).end(result, 'binary');
 };
 
 export default handler;
