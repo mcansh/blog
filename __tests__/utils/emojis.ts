7e5fb77fc84568f5f8a9b41e81355ae243491885
Logan McAnsh <logan@mcan.sh>
2020-10-19 10:45:38 -0400

    test: restore original Math after finishing test
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- __tests__/utils/emojis.ts
+++ __tests__/utils/emojis.ts
@@ -1,9 +1,17 @@
 import randomEmoji from '~/utils/emojis';
 
-it('returns a "random" emoji from the list', () => {
+let originalMath: Math;
+
+beforeAll(() => {
   const mockMath = Object.create(global.Math);
   mockMath.random = () => 0.1;
   global.Math = mockMath;
+});
 
+afterAll(() => {
+  global.Math = originalMath;
+});
+
+it('returns a "random" emoji from the list', () => {
   expect(randomEmoji()).toBe('🙌');
 });
