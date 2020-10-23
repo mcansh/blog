5c556767235adb4a04818d767426e1ec578d116b
Logan McAnsh <logan@mcan.sh>
2020-10-22 21:11:32 -0400

    chore(layouts/post): move event listener inside useEffect to avoid using useCallback hook
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- components/use-scroll-progress.tsx
+++ components/use-scroll-progress.tsx
@@ -3,20 +3,20 @@ import React from 'react';
 const useScrollProgress = () => {
   const [scrollProgress, setScrollProgress] = React.useState(0);
 
-  const handleScroll = React.useCallback(() => {
-    const scroll = window.pageYOffset;
-    const bodyHeight = document.body.offsetHeight;
-    const windowHeight = window.innerHeight;
-    const scrollPercent = (scroll / (bodyHeight - windowHeight)) * 100;
-    const minMaxScroll = Math.min(100, Math.max(0, scrollPercent));
-    setScrollProgress(minMaxScroll);
-  }, []);
-
   React.useEffect(() => {
+    const handleScroll = () => {
+      const scroll = window.pageYOffset;
+      const bodyHeight = document.body.offsetHeight;
+      const windowHeight = window.innerHeight;
+      const scrollPercent = (scroll / (bodyHeight - windowHeight)) * 100;
+      const minMaxScroll = Math.min(100, Math.max(0, scrollPercent));
+      setScrollProgress(minMaxScroll);
+    };
+
     window.addEventListener('scroll', handleScroll, { passive: true });
     handleScroll();
     return () => window.removeEventListener('scroll', handleScroll);
-  }, [handleScroll]);
+  }, []);
 
   return scrollProgress;
 };
