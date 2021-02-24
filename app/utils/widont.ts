b7c4d786de8c1a80641ff478d09c6cdb16dd150b
Logan McAnsh <logan@mcan.sh>
2021-02-23 19:21:43 -0500

    feat: update widont to not do anything when the last 2 words are longer than 15 characters
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/utils/widont.ts
+++ app/utils/widont.ts
@@ -1,9 +1,16 @@
 /**
  * prevent widows
- * https://github.com/daneden/daneden.me/blob/aef50f0c54ebddd138e70d6a3ff711b184a8092f/src/utils/widont.ts
+ * https://github.com/daneden/daneden.me/blob/65cf4b7ec34629fefaf72a72cfa7440403bf1bd7/src/utils/widont.ts
  */
 function widont(subject: string): string {
-  return subject.replace(/ ([^ ]*)$/, '\u00A0$1');
+  const words = subject.split(' ');
+  const lastTwo = words.slice(-2).join(' ');
+
+  if (lastTwo.length >= 15) {
+    return subject;
+  } else {
+    return subject.replace(/ ([^ ]*)$/, '\u00A0$1');
+  }
 }
 
 export { widont };
