7decd668b78284b7ba7ab48309477924211aede4
Logan McAnsh <logan@mcan.sh>
2022-02-09 11:41:47 -0500

    chore: re-add fathom
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,10 @@
 import ReactDOM from 'react-dom';
 import { RemixBrowser } from '@remix-run/react';
+import * as Fathom from 'fathom-client';
+
+Fathom.load('ROTOLYJX', {
+  excludedDomains: ['localhost'],
+  url: `https://kind-thirtyeight.mcansh.blog/script.js`,
+});
 
 ReactDOM.hydrate(<RemixBrowser />, document);
